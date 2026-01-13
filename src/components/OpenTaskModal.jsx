cf03494b4e0bca6a29cabba281355a5eee077d5f
bielmeierwebdev <bielmeierwebdevelop@gmail.com>
2026-01-13 12:40:11 +0100

    update

--- src/components/OpenTaskModal.jsx
+++ src/components/OpenTaskModal.jsx
@@ -1,61 +1,63 @@
-import { Modal, Dialog, DialogTitle, DialogContent, DialogActions, Typography } from "@mui/material";
+import {
+  Modal,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+  Typography,
+  TextField,
+  Button,
+} from "@mui/material";
 
-function OpenTaskModal({open, onClose}){
-    return (
-        <Dialog
-        open={open}
-        onClose={onClose}
-        maxWidth="md"
-        fullWidth
-      >
-        <DialogTitle>
-          <Typography fontWeight={600}>
-            Aufgabe
-          </Typography>
-        </DialogTitle>
+function OpenTaskModal({ open, onClose }) {
+  return (
+    <Dialog open={open} onClose={onClose} maxWidth="md" fullWidth>
+      <DialogTitle>
+        <Typography fontWeight={600}>Aufgabe</Typography>
+      </DialogTitle>
 
-        <DialogContent dividers>
-          <Typography mb={2}>
-            Bitte wähle einen Mitarbeiter aus, für den ein Stundenzettel
-            erstellt werden soll:
-          </Typography>
+      <DialogContent dividers>
+        <Typography mb={2}>
+          Bitte wähle einen Mitarbeiter aus, für den ein Stundenzettel erstellt
+          werden soll:
+        </Typography>
 
-          <TextField
-            select
-            label="Mitarbeiter"
-            value={activeCoWorker}
-            onChange={(e) => setActiveCoWorker(e.target.value)}
-            fullWidth
-            required
-            error={!activeCoWorker}
-            helperText={
-              !activeCoWorker ? "Bitte einen Mitarbeiter auswählen" : ""
-            }
-          >
-            {coWorkerData.map((name) => (
+        <TextField
+          select
+          label="Mitarbeiter"
+          //value={}
+          //onChange={(e) => setActiveCoWorker(e.target.value)}
+          fullWidth
+          required
+          //error={!activeCoWorker}
+          //</DialogContent>helperText={
+          //!activeCoWorker ? "Bitte einen Mitarbeiter auswählen" : ""
+          //}
+        >
+          {/*coWorkerData.map((name) => (
               <MenuItem key={name} value={name}>
                 {name}
               </MenuItem>
-            ))}
-          </TextField>
-        </DialogContent>
+            ))*/}
+        </TextField>
+      </DialogContent>
 
-        <DialogActions>
-          <Button onClick={onClose}>Abbrechen</Button>
-          <Button
-            variant="contained"
-            color="success"
-            onClick={() => {
-              setOpenTimeSheet(true);
-              onClose();
-            }}
-            disabled={!activeCoWorker}
-          >
-            Weiter
-          </Button>
-        </DialogActions>
-      </Dialog>
-    )
+      <DialogActions>
+        <Button onClick={onClose}>Abbrechen</Button>
+        <Button
+          variant="contained"
+          color="success"
+          //onClick={() => {
+          //setOpenTimeSheet(true);
+          //onClose();
+          //}}
+          //disabled={!activeCoWorker}
+        >
+          Weiter
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
 }
 
 export default OpenTaskModal;
